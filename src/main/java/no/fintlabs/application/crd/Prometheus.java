ecc545861836eb5a26fdf0836ca433d4aa17a73f
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2022-11-02 16:10:33 +0100

    Start working on prometheus support

--- src/main/java/no/fintlabs/application/crd/Prometheus.java
+++ src/main/java/no/fintlabs/application/crd/Prometheus.java
@@ -2,13 +2,31 @@ package no.fintlabs.application.crd;
 
 import lombok.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Prometheus {
+
     private boolean enabled;
-    private String path;
-    private String port;
+
+    @Builder.Default
+    private String path = "/actuator/prometheus";
+
+    @Builder.Default
+    private String port = "8080";
+
+    public  Map<String, String> getPrometheusAnnotations() {
+        Map<String, String> annotations = new HashMap<>();
+
+        annotations.put("prometheus.io/scrape", String.valueOf(enabled));
+        annotations.put("prometheus.io/port", port);
+        annotations.put("prometheus.io/path", path);
+
+        return annotations;
+    }
 }
