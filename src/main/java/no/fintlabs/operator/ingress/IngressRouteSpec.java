b1e4475d27e6bb93af6bc79a2b7a4454c4d221e5
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-05-07 13:49:43 +0200

    Fixed issue with middelwares

--- src/main/java/no/fintlabs/operator/ingress/IngressRouteSpec.java
+++ src/main/java/no/fintlabs/operator/ingress/IngressRouteSpec.java
@@ -3,6 +3,7 @@ package no.fintlabs.operator.ingress;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +35,7 @@ public class IngressRouteSpec {
 
     @Data
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class Middleware {
         private String name;
 
