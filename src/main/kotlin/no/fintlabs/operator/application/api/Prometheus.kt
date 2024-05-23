259a74fc43b823f9d861f9136c4766f25d177096
Mattis Bratland <mattis.bratland@novari.no>
2024-05-23 19:36:43 +0200

    Remove unnecessary code

--- src/main/kotlin/no/fintlabs/operator/application/api/Prometheus.kt
+++ src/main/kotlin/no/fintlabs/operator/application/api/Prometheus.kt
@@ -4,16 +4,4 @@ data class Prometheus(
     val enabled: Boolean = true,
     val path: String = "/actuator/prometheus",
     val port: String = "8080"
-) {
-
-    fun getPrometheusAnnotations(): Map<String, String> {
-        val annotations: MutableMap<String, String> = HashMap()
-
-        if (enabled) {
-            annotations["prometheus.io/scrape"] = enabled.toString()
-            annotations["prometheus.io/port"] = port
-            annotations["prometheus.io/path"] = path
-        }
-        return annotations
-    }
-}
+)
