9ddb9a7d03c8e4e45c47a68b0e6b094eb53520c8
Mattis Bratland <mattis.bratland@novari.no>
2024-09-17 12:03:54 +0200

    Move dependencies to libs.version.toml since dependabot does not support gragle.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,1 @@
 rootProject.name = "flaiserator"
-
-pluginManagement {
-    val kotlinVersion: String by settings
-    val fabric8Version: String by settings
-    plugins {
-        kotlin("jvm") version kotlinVersion
-        kotlin("kapt") version kotlinVersion
-        id("io.fabric8.java-generator") version fabric8Version
-    }
-}
