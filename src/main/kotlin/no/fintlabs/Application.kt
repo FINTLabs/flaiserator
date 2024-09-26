4feaca120c72bfe395e40255fb9e3657e1d4d337
Mattis Bratland <mattis.bratland@novari.no>
2024-09-26 21:28:14 +0200

    Remove jackson mixin for Quantity due to complications
    Quantity mixin updated all Quantities in crd to be binary when updating status. This made crds difficult to update

--- src/main/kotlin/no/fintlabs/Application.kt
+++ src/main/kotlin/no/fintlabs/Application.kt
@@ -1,14 +1,13 @@
 package no.fintlabs
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.fabric8.kubernetes.api.model.Quantity
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.fabric8.kubernetes.client.KubernetesClientBuilder
 import io.fabric8.kubernetes.client.utils.KubernetesSerialization
 import io.javaoperatorsdk.operator.Operator
 import io.javaoperatorsdk.operator.api.config.ConfigurationService
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler
 import no.fintlabs.operator.applicationReconcilerModule
-import no.fintlabs.serialization.QuantityMixIn
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import org.koin.mp.KoinPlatform.getKoin
@@ -28,7 +27,6 @@ val baseModule = module {
     single(createdAtStart = true) { defaultConfig() }
     single {
         ObjectMapper().apply {
-            addMixIn(Quantity::class.java, QuantityMixIn::class.java)
             registerKotlinModule()
         }
     }
