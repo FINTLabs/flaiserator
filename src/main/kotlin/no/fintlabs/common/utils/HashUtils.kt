2a1e2ca141896c4b64766d3e04433ed0b43803c6
Mattis Bratland <murillio4@users.noreply.github.com>
2026-08-18 14:07:01 +0200

    fix(utils): ObjectMapper explicit json settings deprecated (#247)
    Use JsonMapper instead of ObjectMapper due to explicit json mapping feature

--- src/main/kotlin/no/fintlabs/common/utils/HashUtils.kt
+++ src/main/kotlin/no/fintlabs/common/utils/HashUtils.kt
@@ -3,14 +3,18 @@ package no.fintlabs.common.utils
 import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.security.MessageDigest
 
 private val mapper =
-    ObjectMapper()
-        .registerKotlinModule()
+    JsonMapper
+        .builder()
+        .addModule(kotlinModule { })
         .configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true)
         .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true)
+        .build()
 
 fun createHash(map: Map<*, *>): String {
     val json = mapper.writeValueAsBytes(map)
